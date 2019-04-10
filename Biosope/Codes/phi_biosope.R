4abc6e881c98f324c9acb44ca7c0f888386149f7
Flavi1P <flavien.petit@obs-vlfr.fr>
2019-04-10 14:21:00 +0200

    add computation of rmse

--- Biosope/Codes/phi_biosope.R
+++ Biosope/Codes/phi_biosope.R
@@ -43,4 +43,57 @@ biosope_calibration <- biosope_calibration %>% mutate(ratio_model = micro * phi_
 ggplot(biosope_calibration)+
   geom_density(aes(x =ratio_model))
 
+#test is on sampled dataset####
 
+biosope <- read_csv("Biosope/Data/biosope")
+
+biosope$optical_layer <- round(biosope$optical_layer)
+
+biosope <- filter(biosope, optical_layer < 4)
+
+biosope_train <- sample_frac(biosope, 0.8)
+biosope_test <- sample_frac(biosope, 0.2)
+
+phi_biosope <- phi_boot(biosope_train, "fluo_urel")
+
+ggplot(phi_biosope, aes(x=size, y = phi, fill = as.factor(optical_layer))) +
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin = phi-se, ymax = phi+se),position=position_dodge())+
+  scale_fill_viridis_d( name = "optical layer")+
+  ylab("Phi")+ xlab("size classe")+
+  ggtitle("Fluorescent yield, Biosope")
+
+phi_biosope <- phi_biosope %>% select(phi, optical_layer, size) %>% spread(key = size, value = phi)
+names(phi_biosope) <- c("optical_layer", "phi_micro", "phi_nano", "phi_pico") 
+
+biosope_calibration <- biosope_test %>% select(micro, nano, pico, amicro, anano, apico, tchla, fluo_urel, optical_layer) %>% 
+  left_join(phi_biosope) %>% 
+  mutate(fluo_calibrate = (fluo_urel/(micro*amicro * phi_micro + nano*anano * phi_nano + pico*apico * phi_pico)))
+
+a <- rmse(biosope_calibration$tchla, biosope_calibration$fluo_calibrate)
+b <- rmse(biosope_calibration$tchla, biosope_calibration$fluo_urel)
+a/b
+
+rmse_computed <- data.frame("run" = c(1:100), "rmse" = 0)
+
+for (i in rmse_computed$run){
+  
+  biosope_train <- sample_frac(biosope, 0.8)
+  biosope_test <- sample_frac(biosope, 0.2)
+  
+  phi_biosope <- phi_boot(biosope_train, "fluo_urel")
+  
+  phi_biosope <- phi_biosope %>% select(phi, optical_layer, size) %>% spread(key = size, value = phi)
+  names(phi_biosope) <- c("optical_layer", "phi_micro", "phi_nano", "phi_pico") 
+  
+  biosope_calibration <- biosope_test %>% select(micro, nano, pico, amicro, anano, apico, tchla, fluo_urel, optical_layer) %>% 
+    left_join(phi_biosope, by = "optical_layer") %>% 
+    mutate(fluo_calibrate = (fluo_urel/(micro*amicro * phi_micro + nano*anano * phi_nano + pico*apico * phi_pico)))
+  
+  a <- rmse(biosope_calibration$tchla, biosope_calibration$fluo_calibrate)
+  b <- rmse(biosope_calibration$tchla, biosope_calibration$fluo_urel)
+  rmse_computed$rmse[i] <- a/b
+  
+}
+mean(rmse_computed$rmse)
+sd(rmse_computed$rmse)

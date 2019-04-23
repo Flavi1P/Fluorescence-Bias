d801b761325de08033ce504b5c4b0c7200afff09
Flavi1P <flavien.petit@obs-vlfr.fr>
2019-04-23 16:44:50 +0200

    fixing minor bug

--- Global_Dataset/Codes/data_viz.R
+++ Global_Dataset/Codes/data_viz.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(FactoMineR)
 
 pigments <- c("fuco", "peri", "hex", "but", "allo", "tchlb", "zea")
 
@@ -26,22 +27,49 @@ dataset_tall <- bind_rows(argo, biosope, boussole) %>%
   gather(key = "size_class", value = "frequence", 1:3) %>% 
   ungroup()
 
-ggplot(dataset)+
-  geom_violin(aes(x = campagne, y = frequence, fill = frequence))+
-  facet_grid(vars(size_class))+
-  theme_bw()
-
-ggplot(dataset)+
+ggplot(dataset_tall)+
   geom_boxplot(aes(x = size_class, y = frequence, fill = campagne))+
   scale_fill_brewer(palette = "Set1")+
   ylim(0,1)+ylab("fraction")+
   theme_bw()
 
-ggplot(dataset)+
+ggplot(dataset_tall)+
   geom_boxplot(aes(x = campagne, y = ratio))+
   ylim(0,15)+
   theme_bw()
 
 
+dataset <- bind_rows(argo, biosope, boussole) %>%
+  mutate(pigsum = rowSums(.[, 1:7])) %>%
+  filter(pigsum != 0)
 
 
+afc <- CA(select(dataset, pigments), row.sup = as.numeric(rownames(dataset[dataset$campagne != "argo",])))
+
+scores <- data.frame(afc$row$coord)
+scores_sup <- data.frame(afc$row.sup$coord)
+scores <- bind_rows(scores, scores_sup)
+
+dataset <- bind_cols(dataset,scores)
+
+pigscore <- data.frame(afc$col$coord)
+
+ggplot()+
+  geom_point(aes(x = Dim.1, y = Dim.2, colour = campagne), data = dataset)+
+  geom_segment(aes(x = 0, xend = Dim.1, y = 0, yend = Dim.2), data = pigscore)+
+  geom_text(aes(x = Dim.1, y = Dim.2, label = rownames(pigscore)), data = pigscore)+
+  scale_color_brewer(palette = "Set1")+
+  theme_bw()
+
+biosope <- filter(dataset, campagne == "biosope") %>% 
+  mutate(coord = paste(round(Dim.1,1), round(Dim.2,1), sep = ";"),
+         dup = duplicated(coord)) %>% 
+  filter(dup == "FALSE")
+
+ggplot()+
+  geom_point(aes(x = Dim.1, y = Dim.2, colour = campagne), data = dataset)+
+  geom_point(aes(x = Dim.1, y = Dim.2), data = biosope, colour = "blue")+
+  geom_segment(aes(x = 0, xend = Dim.1, y = 0, yend = Dim.2), data = pigscore)+
+  geom_text(aes(x = Dim.1, y = Dim.2, label = rownames(pigscore)), data = pigscore)+
+  scale_color_brewer(palette = "Set1")+
+  theme_bw()

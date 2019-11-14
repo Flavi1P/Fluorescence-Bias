2e526396d1033f0611ac78c119fe25a8775ea8e1
Flavi1P <flavien.petit@obs-vlfr.fr>
2019-11-14 17:50:00 +0100

    correction factor by region

--- DB_climato/correction.R
+++ DB_climato/correction.R
@@ -28,8 +28,8 @@ sarc$lovbio <- "lovbio_sarc"
 argo <- argo %>% filter(code != "SARC")
 argo <- bind_rows(argo, sarc)
 
-region_argo <- argo  %>% mutate(ratio_clean = corrected_fluo/tchla, ratio = fluo/tchla) %>%  group_by(code) %>% summarise_at(vars(c(ratio, ratio_clean)), c(mean, sd), na.rm = TRUE) %>% ungroup()
-names(region_argo) <- c("code", "mean_ratio", "mean_ratio_clean", "sd_ratio", "sd_ratio_clean")
+region_argo <- argo  %>% mutate(ratio_clean = corrected_fluo/tchla, ratio = fluo/tchla) %>%  group_by(code) %>% summarise_at(vars(c(ratio, ratio_clean, fitted_math)), c(mean, sd), na.rm = TRUE) %>% ungroup()
+names(region_argo) <- c("code", "mean_ratio", "mean_ratio_clean", "mean_factor", "sd_ratio", "sd_ratio_clean", "sd_factor")
 
 region_argo <- left_join(region_argo, resume_region)
 
@@ -51,6 +51,18 @@ ggplot(region_argo)+
 ggplot(region_argo)+
   geom_col(aes(x = reorder(code, mean_ratio), y = mean_ratio_clean, fill = code))+
   geom_errorbar(aes(x = code, ymin = mean_ratio_clean - sd_ratio_clean, ymax = mean_ratio_clean + sd_ratio_clean))+
+  xlab("Province océanique")+
+  ylab("Rapport Fluo/Chla")+
+  geom_errorbar(aes(code, ymax = 1, ymin = 1),
+                size=1, linetype = "longdash", inherit.aes = F, width = 1)+
+  guides(fill = FALSE)+
+  scale_fill_brewer(palette = "Dark2")+
+  theme_bw(base_size = 20)+
+  ylim(0,9)
+
+ggplot(region_argo)+
+  geom_col(aes(x = reorder(code, mean_ratio), y = mean_factor, fill = code))+
+  geom_errorbar(aes(x = code, ymin = mean_factor - sd_factor, ymax = mean_factor + sd_factor))+
   xlab("Province océanique")+
   ylab("Rapport Fluo/Chla")+
   geom_errorbar(aes(code, ymax = 1, ymin = 1),

150a5ddcb7ab9716199b9673f82c9f6b92394f16
Flavi1P <flavien.petit@obs-vlfr.fr>
2019-11-21 16:12:48 +0100

    some plot and compute aps

--- DB_climato/explore.R
+++ DB_climato/explore.R
@@ -1,8 +1,55 @@
 library(tidyvers)
 library(FactoMineR)
+library(patchwork)
+library(janitor)
+library(readxl)
 
 hplc <- read_csv("DB_climato/Data/database_final")
+map <- read_csv("Data/map_vec")
+spectre <- read_excel("Biosope/Data/Spectres_annick.xlsx")
+spectre <- clean_names(spectre)
 
+#select the absorbtion at 440 and 470nm
+spectre430<- filter(spectre, lambda == 430)
+spectre532<- filter(spectre, lambda == 532)
+
+
+hplc <- hplc %>% mutate(photo_430 = peri * spectre430$peri + but * spectre430$x19_bf + hex * spectre430$x19_hf + fuco * spectre430$fuco + allo * spectre430$allox + chla * spectre430$chl_a + dv_chla * spectre430$dv_chla,
+                              photo_532 = peri * spectre532$peri + but * spectre532$x19_bf + hex * spectre532$x19_hf + fuco * spectre532$fuco + allo * spectre532$allox + chla * spectre532$chl_a + dv_chla * spectre532$dv_chla)
+
+
+
+hplc <- mutate(hplc, tchla = chla + dv_chla)
+
+ggplot(hplc)+
+  geom_point(aes(x = tchla, y = photo_440, colour = "aps 440"))+
+  geom_point(aes(x = tchla, y = photo_470, colour = "aps 470"))+
+  geom_point(aes(x = tchla, y = photo_430, colour = "aps 430"))+
+  geom_point(aes(x = tchla, y = photo_532, colour = "aps 532"))+
+  scale_color_brewer(palette = "Dark2")+
+  theme_bw()
+  
+  
+hplc$system <- ifelse(hplc$ze_morel > hplc$mld, "Stratified", "Mixed")
+
+ggplot(hplc)+
+  geom_point(aes(x = lon, y = lat, colour = system))+
+  geom_path(aes(x = long, y = lat, group = group), data = map)+
+  coord_quickmap()
+
+ggplot(hplc)+
+  geom_point(aes(x = tchla, y = ratio))+
+  geom_path(aes(x = tchla, y = 3), colour = "red")+
+  scale_color_brewer(palette = "Dark2")+
+  theme_bw()+
+  facet_wrap(.~system)
+
+ggplot(hplc)+
+  geom_point(aes(x = tchla, y = photo_440, colour = "aps 440"))+
+  geom_point(aes(x = tchla, y = photo_470, colour = "aps 470"))+
+  scale_color_brewer(palette = "Dark2")+
+  theme_bw()+
+  facet_wrap(.~system)
 
 model <- lm(ratio~chla + peri + fuco + dv_chla + allo + hex + but, data = hplc)
 summary(model)
